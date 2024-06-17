1ed2094e4c24d34e5cf7e59032a4a31bd31fe763
Mahender Kumar <mksb1947@gmail.com>
2024-06-18 00:38:35 +0530

    .

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\dev\\flutter src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "F:\\mario" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\dev\\flutter src\\flutter"
+  "PROJECT_DIR=F:\\mario"
+  "FLUTTER_ROOT=D:\\dev\\flutter src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=F:\\mario\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=F:\\mario"
+  "FLUTTER_TARGET=F:\\mario\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9lZGQ4NTQ2MTE2NDU3YmRmMWM1YmRmYjEzZWNiOTQ2M2QyYmI1ZWQ0Lw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=F:\\mario\\.dart_tool\\package_config.json"
+)
